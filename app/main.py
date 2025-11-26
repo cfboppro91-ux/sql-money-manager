6c1ff534a6edc41f61924c3e426c8a87d529ceeb
Chau Cong Quan <Ssew9948@gmail.com>
2025-11-26 12:57:07 +0700

    update main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,43 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.openapi.utils import get_openapi
+
 from app.database import Base, engine
 from app.routers import auth, category, wallet, transaction, budget
 
 Base.metadata.create_all(bind=engine)
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
 app = FastAPI()
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="Money Manager API",
+        version="1.0",
+        description="Backend for Money Manager",
+        routes=app.routes,
+    )
+
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+        }
+    }
+
+    for path in openapi_schema["paths"]:
+        for method in openapi_schema["paths"][path]:
+            openapi_schema["paths"][path][method]["security"] = [{"BearerAuth": []}]
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 app.include_router(auth.router)
 app.include_router(category.router)
