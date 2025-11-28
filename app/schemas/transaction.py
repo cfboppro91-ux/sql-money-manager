7c3eb2d017ecb566d9a9f75a70a7feed1c21efc4
Chau Cong Quan <Ssew9948@gmail.com>
2025-11-28 16:53:38 +0700

    update transaction

--- app/schemas/transaction.py
+++ app/schemas/transaction.py
@@ -1,7 +1,7 @@
 # schema/transaction.py
 from pydantic import BaseModel
 from uuid import UUID
-
+from datetime import datetime
 class TransactionBase(BaseModel):
     type: str  # income / expense
     amount: float
